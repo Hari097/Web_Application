a27fe12d7a42ace1c6d749d2b157f2325ff42b36
Hari097 <harikalam143@gmail.com>
2024-03-29 22:31:09 +0530

    login page fetch get data

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,9 +1,41 @@
 let myformEI = document.getElementById("myform")
 
 
-myformEI.addEventListener("submit",submitFrom)
 
 
-function submitFrom(){
-    alert("submit")
+const submitFrom =  async function() {
+    
+
+
+    try{ 
+        let options = {
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
+            mode: "no-cors", // no-cors, *cors, same-origin
+            // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            // credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+              'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        }
+        let URL =  "http://localhost:3005/login" 
+        let response =  await fetch(URL,options) 
+
+        let jsonData = response
+        console.log(jsonData)
+
+
+
+
+       
+    } catch(error){
+            console.log(` fetch error : ${error.message}`)
+
+    }
 }
+
+myformEI.addEventListener("submit",function(event){
+    event.preventDefault()
+    submitFrom()
+    })
+    
