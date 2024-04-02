c9cb05f9dd7cdc5ed124154778d2a90ce98d39b2
Hari097 <harikalam143@gmail.com>
2024-04-02 19:17:33 +0530

    dashboard details

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,14 +1,23 @@
 
 function showUserDetailDashoard(){
-    let usernamedashboard = JSON.parse(localStorage.getItem("UserName"))
+    let getuserlocalstorage = JSON.parse(localStorage.getItem("userdata"))
+    // let usernamedashboard = JSON.parse(localStorage.getItem("UserName"))
 
-    let useremaildashboard = JSON.parse(localStorage.getItem("UserEmail"))
-    
+    // let useremaildashboard = JSON.parse(localStorage.getItem("UserEmail"))
     let dashboardUsernameEl = document.getElementById("getNamelocalstorage")
     let dashboardUseremailEl = document.getElementById("getEmaillocalstorage")
+    let dashboardUserpasswordEl = document.getElementById("getpasswordlocalstorage")
+    let dashboardUsergenderEl = document.getElementById("getgenderlocalstorage")
+    getuserlocalstorage.forEach(element => {
+       dashboardUsernameEl.textContent = element.username 
+       dashboardUseremailEl.textContent = element.useremail 
+       dashboardUserpasswordEl.textContent = element.userpassword 
+       dashboardUsergenderEl.textContent = element.usergender
+    });
     
-    dashboardUsernameEl.textContent = usernamedashboard
-    dashboardUseremailEl.textContent = useremaildashboard 
+   
+    // dashboardUsernameEl.textContent = usernamedashboard
+    // dashboardUseremailEl.textContent = useremaildashboard 
     
 } 
 
